84ccfcdb88368e4efedc7599fdc3931466b0dea6
ratchapoltt <ratchapoltt.tum@gmail.com>
2024-08-07 14:37:45 +0700

    perf(module): set up Open API service in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,13 @@ import { Module, OnModuleInit } from "@nestjs/common";
 import * as Joi from "joi";
 
 import { ValidEnvironmentException } from "./exceptions";
+import { OpenApiService } from "./services";
 import { environmentValidationSchema } from "./validations";
 
-@Module({})
+@Module({
+  providers: [OpenApiService],
+  exports: [OpenApiService]
+})
 export class CoreModule implements OnModuleInit {
   private validEnvironment(): void {
     const { error }: Joi.ValidationResult = environmentValidationSchema.validate(environment);
