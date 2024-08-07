28d7bc244dd1169264ebbb907084ad3e2a7fb74b
ratchapoltt <ratchapoltt.tum@gmail.com>
2024-08-07 12:19:40 +0700

    feat(exception): create valid environment exception class

---
+++ src/app/core/exceptions/index.ts
@@ -0,0 +1,1 @@
+export * from "./valid-environment.exception";
---
+++ src/app/core/exceptions/valid-environment.exception.ts
@@ -0,0 +1,5 @@
+export class ValidEnvironmentException extends Error {
+  public constructor(message: string) {
+    super(message);
+  }
+}
