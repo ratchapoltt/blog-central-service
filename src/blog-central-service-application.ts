851c14ec2597b26ebbbaa3c7c8dbadb295e15265
ratchapoltt <ratchapoltt.tum@gmail.com>
2024-08-07 12:37:06 +0700

    perf(main): updated listen port, hostname method, and added a new log message

--- src/blog-central-service-application.ts
+++ src/blog-central-service-application.ts
@@ -1,3 +1,4 @@
+import { environment } from "@environment";
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
@@ -12,8 +13,9 @@ export class BlogCentralServiceApplication {
       const application: NestExpressApplication = await NestFactory.create(AppModule);
 
       await application.init();
-      await application.listen(3000);
+      await application.listen(environment.server.port, environment.server.hostname);
 
+      this.logger.log(`Application active on ${environment.profile} profile`);
       this.logger.log(`Application is running on ${await application.getUrl()}`);
       this.logger.log("Application service started successfully");
     } catch (error) {
